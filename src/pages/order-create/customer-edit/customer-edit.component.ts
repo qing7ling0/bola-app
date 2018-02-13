c4530a4dfdd17704a1938c3ae14f522993aaebe3
qing7ling0 <975604172@qq.com>
2018-02-13 21:37:20 +0800

    bola-app

--- src/pages/order-create/customer-edit/customer-edit.component.ts
+++ src/pages/order-create/customer-edit/customer-edit.component.ts
@@ -1,40 +1,67 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { CustomerData } from '../../../interface/customer-data'
+import { FormValidator } from '../../../utils/form-validator'
+
+const FORM_OPTIONS = (data)=> [
+  {key:'phone2', label:'手机号', defaultValue:data&&data.phone||'', validators:[{key:'required', validator:Validators.required}, {key:'pattern', validator:Validators.pattern(/^((\+?86)|(\(\+86\)))?(13[012356789][0-9]{8}|15[012356789][0-9]{8}|18[02356789][0-9]{8}|147[0-9]{8}|1349[0-9]{7})$/)}]},
+  {key:'phone', label:'手机号', defaultValue:data&&data.phone||'', validators:[{key:'required', validator:Validators.required}, {key:'pattern', validator:Validators.pattern(/^((\+?86)|(\(\+86\)))?(13[012356789][0-9]{8}|15[012356789][0-9]{8}|18[02356789][0-9]{8}|147[0-9]{8}|1349[0-9]{7})$/)}]},
+  {key:'name', label:'姓名', defaultValue:data&&data.name||'', validators:[{key:'required', validator:Validators.required}]},
+  {key:'weixin', label:'微信号', defaultValue:data&&data.weixin||'', validators:[{key:'required', validator:Validators.required}]},
+
+  {key:'country', label:'国家', defaultValue:data&&data.country||'', validators:[]},
+  {key:'city', label:'城市', defaultValue:data&&data.city||'', validators:[]},
+  {key:'address', label:'地址', defaultValue:data&&data.address||'', validators:[]},
+  {key:'zipcode', label:'邮编', defaultValue:data&&data.zipcode||'', validators:[]},
+]
 
 @Component({
   selector: 'order-customer-edit',
   templateUrl: 'customer-edit.html'
 })
 export class OrderCustomerEditComponent implements OnInit {
-  @Input() customerData: CustomerData;
+  customerData: CustomerData;
+  @Input() onChange: Function;
   customerGroup: FormGroup;
+  formOptions: Array<any>
 
   constructor(
     public navCtrl: NavController,
     private formBuilder: FormBuilder,
+    private toastCtrl: ToastController
   ) {}
 
   ngOnInit(): void {
-    this.customerGroup = this.formBuilder.group({
-      phone: ['', [Validators.required]],
-      name: ['', [Validators.required]],
-      weixin: ['', [Validators.required]],
-      
-      country: [''],
-      city: [''],
-      address: [''],
-      zipcode: [''],
-    });
+    this.formOptions = FORM_OPTIONS(null);
+    this.customerGroup = this.formBuilder.group(FormValidator.getFormBuildGroupOptions(this.formOptions));
   }
 
   onPhoneSureBtnClicked() {
-    
+    if (this.onChange) {
+      this.onChange(this);
+    }
+  }
+
+  submit() {
+    if (!this.customerGroup.valid) {
+      let message = FormValidator.getValidError(this.customerGroup.controls, this.formOptions);
+      if (message) {
+        this.toastCtrl.create({
+          message:message,
+          duration:1500,
+          position:'middle'
+        }).present();
+      }
+      return null;
+    }
+
+    return this.customerGroup.value;
   }
 
   onNavClicked(nav: object) : void {
+
   }
 
 }
---
+++ src/utils/form-validator.ts
@@ -0,0 +1,39 @@
+import { FormControl, Validators, AbstractControl } from '@angular/forms';
+
+export class FormValidator {
+
+  static getFormBuildGroupOptions(options: Array<any>) {
+    let ret = {};
+    options.forEach((item)=> {
+      let validators = item.validators.map((va)=> {
+        return va.validator;
+      })
+      ret[item.key] = [item.defaultValue, validators];
+    })
+
+    return ret;
+  }
+
+  static getValidError(fromControls: {[key: string]: AbstractControl}, options: Array<any>) {
+    for(let op of options) {
+      let ctrl = fromControls[op.key];
+      if (ctrl) {
+        for(let va of op.validators) {
+          if (ctrl.hasError(va.key)) {
+            return va.message || FormValidator.getError(va.key, op.label);
+          }
+        }
+      }
+    }
+
+    return '';
+  }
+
+  static getError(validType: string, label: string) {
+    if (validType === 'required') {
+      return `请填写${label}`;
+    } else {
+      return `${label}格式不合法`
+    }
+  }
+}
